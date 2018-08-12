4cc9687b2e08722b044f9c58214db6c157bb461e
DaveDLeague <dave.dunn@jointheleague.org>
2018-08-12 12:14:35 -0700

    Update _04_SearchTest.java

--- src/_01_Searching_Algorithms/_04_SearchTest.java
+++ src/_01_Searching_Algorithms/_04_SearchTest.java
@@ -9,24 +9,24 @@ class _04_SearchTest {
 	// A MINIMUM OF 3 TESTS ARE REQUIRED FOR EACH METHOD
 	
 	@Test
-	void testLinearSearch() {
+	public void testLinearSearch() {
 		//1. use the assertEquals method to test your linear search method.
 	}
 
 	@Test
-	void testBinarySearch() {
+	public void testBinarySearch() {
 		//2. use the assertEquals method to test your binary search method.
 		//   remember that the array must be sorted
 	}
 	
 	@Test
-	void testInterpolationSearch() {
+	public void testInterpolationSearch() {
 		//3. use the assertEquals method to test your interpolation search method.
 		//   remember that the array must be sorted and evenly distributed
 	}
 	
 	@Test
-	void testExponentialSearch() {
+	public void testExponentialSearch() {
 		//4. use the assertEquals method to test your exponential search method.
 		//   remember that the array must be sorted
 	}
