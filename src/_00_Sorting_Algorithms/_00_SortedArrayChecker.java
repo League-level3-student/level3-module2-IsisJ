c3fc57dab739e0b13267ea4e9c3c284ebc20d510
tactreconx <league@WTS-MM-11.local>
2019-05-11 12:18:31 -0700

    Compare chars

--- src/_00_Sorting_Algorithms/_00_SortedArrayChecker.java
+++ src/_00_Sorting_Algorithms/_00_SortedArrayChecker.java
@@ -1,13 +1,48 @@
 package _00_Sorting_Algorithms;
 
+
 public class _00_SortedArrayChecker {
+
+	public static boolean intArraySorted(int[] unsorted) {
+		for(int i = 0; i<unsorted.length-1; i++) {
+			int currentNum = unsorted[i];
+			int comparedNum = unsorted[i+1];
+			if(currentNum > comparedNum) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	public static boolean doubleArraySorted(double[] unsorted) {
+		for(int i = 0; i<unsorted.length-1; i++) {
+			double currentNum = unsorted[i];
+			double comparedNum = unsorted[i+1];
+			if(currentNum > comparedNum) {
+				return false;
+			}
+		}
+		return true;	
+		}
+
+	public static boolean charArraySorted(char[] unsorted) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public static boolean stringArraySorted(String[] unsorted) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	
+	
 	//1. Write a static method called intArraySorted. 
 	//   This method takes in an array of integers
 	//   and it returns a boolean.
 	//   The method returns true if the integer
 	//   array is in ascending order and false otherwise
 
-	
 	//2. Write a static method called doubleArraySorted. 
 	//   This method takes in an array of doubles
 	//   and it returns a boolean.
