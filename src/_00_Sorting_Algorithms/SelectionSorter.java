2c62fb2eaf5882afecfb3b45d3afd30966d5f15e
IsisJ <league@Mac-mini-3.local>
2019-06-15 12:21:48 -0700

    finished

--- src/_00_Sorting_Algorithms/SelectionSorter.java
+++ src/_00_Sorting_Algorithms/SelectionSorter.java
@@ -11,17 +11,25 @@ public class SelectionSorter extends Sorter {
 	void sort(int[] array, SortingVisualizer display) {
 		//1. make a for loop to iterate through all but the last 
 		//   element of the array
-		
+		for(int i=0; i<array.length-1; i++) {
 			//2. create an integer called index and set it equal to i
-			
+			int index=i;
 			//3. make another for loop that starts at i + 1 and
 			//   goes through the entire array
-			
+			for(int j=i+1; j<array.length; j++) {
 				//4. if the array element at j is less than the 
 				//   element at index, then set index equal to j
-				
+				if(array[j]< array[index]) {
+					index=j;
+				}
 			//5. swap the array element at index with the array element at i
-			
-		
+				
+			}
+			_01_SwappingDigits.swapIndex(array,index,i);
+			display.updateDisplay();
+		}
 	}
+	
+	
+	
 }
