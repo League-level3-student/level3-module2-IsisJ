b7efa29556ec8152674304761c909c07d090f36c
IsisJ <isisofalexandria@gmail.com>
2019-09-12 20:30:43 -0700

    took out some comments, ready to merge

--- src/_01_Searching_Algorithms/_01_BinarySearch.java
+++ src/_01_Searching_Algorithms/_01_BinarySearch.java
@@ -1,37 +1,19 @@
 package _01_Searching_Algorithms;
 
 public class _01_BinarySearch {
-	// Binary searches are good for sorted data because it
-	// can potentially take a lot less time than a linear search,
-	// especially if the data set is very large. But it cannot be used
-	// for unsorted lists.
-	
-	// Because binary search is a recursive algorithm, the
-	// binarySearch method will take in two extra integer parameters
-	// to define the start and end position of the search.
-	
-	//1. Complete the steps in the binarySearch method.
-	//   We can assume array is already sorted
+
 	public static int binarySearch(int[] array, int start, int end, int value) {
 
 		if(end >= start) {
-			
-		
-
-
 			int mid = ((end - start)/2)+start;
 			
 
 			if(array[mid] == value) {
 				return mid;
 			}
-
-            
 			if(array[mid]>value) {
 				return binarySearch(array, start, mid-1, value);
 			}
-
-			
 			return binarySearch(array, mid+1, end, value);
 			
 		}
--- src/_01_Searching_Algorithms/_04_SearchTest.java
+++ src/_01_Searching_Algorithms/_04_SearchTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.Test;
 
 class _04_SearchTest {
 
-	// A MINIMUM OF 3 TESTS ARE REQUIRED FOR EACH METHOD
 	
 	@Test
 	public void testLinearSearch() {
@@ -20,8 +19,7 @@ class _04_SearchTest {
 
 	@Test
 	public void testBinarySearch() {
-		//2. use the assertEquals method to test your binary search method.
-		//   remember that the array must be sorted
+		
 		int[] numbers = {22, 25, 30, 40, 45};
 		
 		assertEquals(2, _01_BinarySearch.binarySearch(numbers, 0, 4, 30));
@@ -31,8 +29,7 @@ class _04_SearchTest {
 	
 	@Test
 	public void testInterpolationSearch() {
-		//3. use the assertEquals method to test your interpolation search method.
-		//   remember that the array must be sorted and evenly distributed
+		
 		int[] numbers = {22, 25, 30, 40, 45};
 		
 		assertEquals(2, _02_InterpolationSearch.interpolationSearch(numbers, 30));
@@ -44,8 +41,7 @@ class _04_SearchTest {
 	
 	@Test
 	public void testExponentialSearch() {
-		//4. use the assertEquals method to test your exponential search method.
-		//   remember that the array must be sorted
+		
 		int[] numbers = {1,2,3,4,45,67,74,89,98,123,234,345,456};
 		
 		assertEquals(7, _03_ExponentialSearch.exponentialSearch(numbers,89));
